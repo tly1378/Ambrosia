74f77f7ab7ea77f76a2253694e84b2a62c01c025
Tan Liyuan <tanliyuan1378@gmail.com>
2023-02-02 23:28:03 +0800

    添加单例基类

---
+++ Singleton.cs
@@ -0,0 +1,8 @@
+﻿namespace Tabler
+{
+    public class Singleton<T> where T : new()
+    {
+        private static readonly Lazy<T> lazy = new Lazy<T>(() => new T());
+        public static T Instance { get => lazy.Value; }
+    }
+}
