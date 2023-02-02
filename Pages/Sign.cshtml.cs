4c44a2bdffb413ae179bd369d1a57cd5dd123ce3
Tan Liyuan <tanliyuan1378@gmail.com>
2023-02-02 21:13:35 +0800

    添加注册登录页面，添加隐藏页眉页脚功能。

---
+++ Pages/Sign.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tabler.Pages
+{
+    public class SignModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
