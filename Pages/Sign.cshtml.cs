62d8223a9c8748d756a5eea6b47df8e21a78bd91
Tan Liyuan <tanliyuan1378@gmail.com>
2023-02-02 23:04:36 +0800

    完善注册登录页面

--- Pages/Sign.cshtml.cs
+++ Pages/Sign.cshtml.cs
@@ -5,9 +5,21 @@ namespace Tabler.Pages
 {
     public class SignModel : PageModel
     {
+        [BindProperty] public string Email { get; set; } = "";
+        [BindProperty] public string Password { get; set; } = "";
+
         public void OnGet()
         {
 
+        }
+
+        public void OnPostSignIn()
+        {
+            Console.WriteLine(
+                "New User:\n" +
+                $"\t{Email}\n" +
+                $"\t{Password}"
+                );
         }
     }
 }
