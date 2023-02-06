7150f3bd5b0f07a30748c5bbc182c034a54cb84a
Tan Liyuan <tanliyuan1378@gmail.com>
2023-02-07 00:07:24 +0800

    添加配置文件读取机制

---
+++ Configuration.cs
@@ -0,0 +1,41 @@
+﻿namespace Tabler
+{
+    public class Configuration : Singleton<Configuration>
+    {
+        IConfiguration configuration;
+
+        public Configuration()
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("Properties/settings.json");
+            configuration = configurationBuilder.Build();
+        }
+
+        public string this[string key] => configuration[key];
+
+
+        public T Get<T>(string key)
+        {
+            string value = configuration[key];
+            if (typeof(T) == typeof(string))
+            {
+                if (value is T result)
+                    return result;
+            }
+            else if (typeof(T) == typeof(int))
+            {
+                int valueInt = int.Parse(value);
+                if (valueInt is T result)
+                    return result;
+            }
+            else if (typeof(T) == typeof(float))
+            {
+                float valueInt = float.Parse(value);
+                if (valueInt is T result)
+                    return result;
+            }
+            throw new InvalidOperationException($"{key} is not a {nameof(T)}.");
+        }
+    }
+}
